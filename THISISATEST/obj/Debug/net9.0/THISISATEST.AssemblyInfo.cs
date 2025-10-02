fcf53bd27e7bc3668a429ddae198c7da671c61d8
dev-mcknz <dev.mcknz@gmail.com>
2025-10-02 14:24:47 -0400

    this is a test of mackenzie pushing a branch yay

--- THISISATEST/obj/Debug/net9.0/THISISATEST.AssemblyInfo.cs
+++ THISISATEST/obj/Debug/net9.0/THISISATEST.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("THISISATEST")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a846e153bd538fb8383025dcf9498508401a94b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("THISISATEST")]
 [assembly: System.Reflection.AssemblyTitleAttribute("THISISATEST")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
